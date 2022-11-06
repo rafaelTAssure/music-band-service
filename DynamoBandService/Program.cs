efe39dde699de515882ab0184372dc0fb050cb4a
Rafael Rojas <rafaelrojas1991@gmail.com>
2022-11-06 19:45:50 -0400

    adding env variables

--- DynamoBandService/Program.cs
+++ DynamoBandService/Program.cs
@@ -1,5 +1,7 @@
+using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -17,17 +19,27 @@ builder.Services.AddCors(p => p.AddPolicy(MyAllowSpecificOrigins, builder =>
     builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
 }));
 
-var awsOptions = builder.Configuration.GetAWSOptions();
+/*var awsOptions = builder.Configuration.GetAWSOptions();
 builder.Services.AddDefaultAWSOptions(awsOptions);
 builder.Services.AddAWSService<IAmazonDynamoDB>();
-builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();*/
+
+//var credentials = new BasicAWSCredentials("AKIAQWGKZBBFCAL36A2K", "lEkLSQiE/TnejCYoVaWVEm51zbnGow9lx3Z0Mfvi");
+
+var credentials = new BasicAWSCredentials(Environment.GetEnvironmentVariable("AWS-ACCESS-KEY-ID"), Environment.GetEnvironmentVariable("AWS-SECRET-KEY-ID"));
+var config = new AmazonDynamoDBConfig()
+{
+    RegionEndpoint = RegionEndpoint.USEast1
+};
+var client = new AmazonDynamoDBClient(credentials, config);
+builder.Services.AddSingleton<IAmazonDynamoDB>(client);
+builder.Services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
+app.UseSwagger();
     app.UseSwaggerUI();
 //}
 
